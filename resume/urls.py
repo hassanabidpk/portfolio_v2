e2557d812ada330e641175b1cc8136a843a4cc9a
Hassan Abid <hassanabidpk89@gmail.com>
2016-08-30 13:02:14 +0900

    improve portfolio layout

--- resume/urls.py
+++ resume/urls.py
@@ -4,8 +4,8 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.about, name='about'),
+    url(r'^$', views.about, name='about'),
+    url(r'^projects/$', views.index, name='index'),
     url(r'^contact/$',views.contact,name='contact'),
     url(r'^contact/thanks/$',views.contact_thanks,name='contact_thanks'),
 
